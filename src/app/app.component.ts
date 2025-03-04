17ed6cb78642bb0d0bd00d6caebb30da73fc42b4
Taner Saydam <taner_and_you@hotmail.com>
2025-03-04 10:33:30 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FlexiButtonComponent } from '../../library/src/lib/flexi-button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FlexiButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'flexi-button';
-}
+export class AppComponent {}
